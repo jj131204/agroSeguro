a523b6d883109b098b1b836b653688ffc4914aef
jarteta2004 <jj131204@gmail.com>
2025-06-20 18:19:35 -0500

    correcciones

--- src/app/templates/profile/profile.component.ts
+++ src/app/templates/profile/profile.component.ts
@@ -1,15 +1,36 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient, HttpClientModule, HttpHeaders } from '@angular/common/http';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
-  styleUrls: ['./profile.component.css']
+  styleUrls: ['./profile.component.css'],
+  imports: [CommonModule, HttpClientModule]
 })
 export class ProfileComponent implements OnInit {
+  finca: any = null;
+  errorMessage = '';
 
-  constructor() { }
+  constructor(private http: HttpClient) {}
 
   ngOnInit() {
+    this.cargarFinca();
   }
 
+  cargarFinca() {
+    const token = localStorage.getItem('token') || '';
+    const headers = new HttpHeaders().set('Authorization', `Bearer ${token}`);
+
+    this.http.get<any>('http://127.0.0.1:8000/api/finca', { headers })
+      .subscribe({
+        next: (data) => {
+          this.finca = data;
+        },
+        error: (error) => {
+          this.errorMessage = 'Error al cargar la finca';
+          console.error(error);
+        }
+      });
+  }
 }
